de8230acfd3926c16837c95f0ca6750e66440e66
Marcela Herrera <marcelaalejandraherrera@yahoo.com.ar>
2022-10-10 12:32:04 -0300

    menu desplegable

--- Assets/js/scripts.js
+++ Assets/js/scripts.js
@@ -1,11 +1,26 @@
 const n = document.querySelector('header nav');
 const burger = document.querySelector('#menu-check');
 
+const excursiones = document.querySelector('#excursiones');
+const excursionesSubmenu = document.querySelector('#excursiones ~ul');
+
+
+
+
 /* mueve el menú fuera del viewport */
 function hideMenu() {
     burger.setAttribute('checked', 'false');
     n.classList.remove("mostrar");
+    // if (e.target == excursiones)
+    //     excursionesSubmenu.classList.toggle("mostrarSubmenu");
 }
+
+
+// function hideMenu() {
+//     burger.setAttribute('checked', 'false');
+//     n.classList.remove("mostrar");
+// }
+
 
 
 //al clickear en el menu hamburguesa le damos una clase al menu de navegacion
@@ -14,8 +29,15 @@ burger.addEventListener("click", () => {
     n.classList.toggle("mostrar");
 });
 
+// al hacer click en excursiones debemos desplegar el submenu
+
 // asociamos un eventListener a cada etiqueta <a>
 // para invocar a hideMenu cuando se clickea en un enlace
 const navLink = document.querySelectorAll('body nav a');
-navLink.forEach((link) =>
-link.addEventListener("click", hideMenu));
+navLink.forEach((link) => {
+    link.addEventListener("click", hideMenu)
+})
+
+// excursiones.addEventListener("click", () => {
+//     excursionesSubmenu.classList.toggle("mostrarSubmenu");
+// })
